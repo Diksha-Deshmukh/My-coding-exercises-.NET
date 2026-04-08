84b3e4af878a42ea49d900d9921fe81b14785302
Diksha Deshmukh <er.dikshadeshmukh@gmail.com>
2026-04-08 10:19:47 +0100

    Time: 1 ms (93.81%) | Memory: 53.3 MB (52.83%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.cs
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) 
+    {
+        return AddRecursive(l1, l2, 0);
+    }
+
+    private ListNode AddRecursive(ListNode l1, ListNode l2, int carry)
+    {
+        if (l1 == null && l2 == null && carry == 0) return null;
+
+        int sum = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+        return new ListNode(sum % 10, AddRecursive(l1?.next, l2?.next, sum / 10));
+    }
+}
