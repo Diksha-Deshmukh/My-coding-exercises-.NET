60e82329cbbb6a5d93c07829ec7cd8e894360a57
Diksha Deshmukh <er.dikshadeshmukh@gmail.com>
2026-05-14 15:59:57 +0100

    Time: 13 ms (87.68%) | Memory: 42.9 MB (81.13%) - LeetSync

--- 16-3sum-closest/3sum-closest.cs
+++ 16-3sum-closest/3sum-closest.cs
@@ -1,108 +1,28 @@
 public class Solution {
-    public int ThreeSumClosest(int[] nums, int target)
- {
-     Suffle(nums);
-     Quick3WaySort(nums, 0, nums.Length - 1);
+    public int ThreeSumClosest(int[] nums, int target) {
+        if(nums.Length<3) return 0;
+        Array.Sort(nums);
+        int closestSum = nums[0]+nums[1]+nums[2];
 
-     int result = 0, rest = int.MaxValue;
-     int sumValue = 0;
-     var tempRest = 0;
-     int lo = 0, hi = 0;
-     for (var index = 0; index < nums.Length - 2; index++)
-     {
-         lo = index + 1;
-         hi = nums.Length - 1;
+        for(int i=0;i<nums.Length;i++){
+            if(i>0 && nums[i]== nums[i-1]) continue;
+            int j=i+1;
+            int k= nums.Length-1;
+            while(j<k){
+                var sum = nums[i]+nums[j]+nums[k];
+                if(sum==target)return sum;
+                
+                if(Math.Abs(target-sum)<Math.Abs(target-closestSum)){
+                    closestSum = sum;
+                }
+                if(sum>target){
+                    k--;
+                }else{
+                    j++;
+                }
+            }
+        }
 
-         while (lo < hi)
-         {
-             sumValue = nums[index] + nums[lo] + nums[hi];
-             tempRest = target - sumValue;
-             if (tempRest == 0) { return sumValue; }
-
-             tempRest = tempRest < 0 ? -tempRest : tempRest;
-             if (tempRest < rest)
-             {
-                 rest = tempRest;
-                 result = sumValue;
-             }
-
-             if (sumValue < target)
-             {
-                 do
-                 {
-                     lo++;
-                 } while (lo < hi && nums[lo - 1] == nums[lo]);
-             }
-             else if (sumValue > target)
-             {
-                 do
-                 {
-                     hi--;
-                 } while (lo < hi && nums[hi + 1] == nums[hi]);
-             }
-             else
-             {
-                 do
-                 {
-                     lo++;
-                 } while (lo < hi && nums[lo - 1] == nums[lo]);
-                 do
-                 {
-                     hi--;
-                 } while (lo < hi && nums[hi + 1] == nums[hi]);
-             }
-         }
-
-         while (index < nums.Length - 3 && nums[index + 1] == nums[index])
-         {
-             index++;
-         }
-     }
-
-     return result;
- }
-
- void Suffle(int[] nums)
- {
-     var random = new Random();
-     int N = nums.Length;
-     int r, temp;
-     for (int i = 0; i < N; i++)
-     {
-         r = random.Next(i + 1);
-
-         temp = nums[r];
-         nums[r] = nums[i];
-         nums[i] = temp;
-     }
- }
-
- void Quick3WaySort(int[] nums, int lo, int hi)
- {
-     if (lo >= hi) { return; }
-     var lt = lo;
-     var gt = hi;
-     var i = lo;
-     var v = nums[i];
-     int temp;
-
-     while (i <= gt)
-     {
-         if (nums[i] > v)
-         {
-             temp = nums[i];
-             nums[i] = nums[gt];
-             nums[gt--] = temp;
-         }
-         else if (nums[i] < v)
-         {
-             temp = nums[i];
-             nums[i] = nums[lt];
-             nums[lt++] = temp;
-         }
-         else { i++; }
-     }
-     Quick3WaySort(nums, lo, lt - 1);
-     Quick3WaySort(nums, gt + 1, hi);
- }
+        return closestSum;
+    }
 }
