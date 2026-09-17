7594b9bd6b274a3d2d1076e12ec05bd00dea5c2c
Diksha Deshmukh <er.dikshadeshmukh@gmail.com>
2026-09-17 18:18:26 +0200

    Time: 0 ms (100.00%) | Memory: 47 MB (37.11%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cs
+++ 42-trapping-rain-water/trapping-rain-water.cs
@@ -1,38 +1,48 @@
+/*
+1. Identify the pointer with the lesser value
+2. Is this pointer value greater than or equal to max on that side
+  yes -> update max on that side
+  no -> get water for pointer, add to total
+3. move pointer inwards
+4. repeat for other pointer
+ */
+
 public class Solution {
     public int Trap(int[] height) {
-        int res = 0;
-        int maxI = 0;
-        int left = 0;
-        int right = 0;
-        for(int i = 1; i < height.Length; i++)
+        var totalWater = 0;
+        var maxRight = 0;
+        var maxLeft = 0;
+        var left = 0;
+        var right = height.Length - 1;
+
+        while (left < right)
         {
-            if(height[i] > height[maxI])
+            if (height[right] > height[left])
             {
-                maxI = i;
-            }
-        }
-        for(int i = 0; i < maxI; i++)
-        {
-            if(height[i] > left)
-            {
-                left = height[i];
+                if (height[left] > maxLeft)
+                    maxLeft = height[left];
+                else
+                {
+                    var currentWater = maxLeft - height[left];
+                    if (currentWater >= 0)
+                        totalWater += currentWater;
+                }
+                left++;
             }
             else
             {
-                res += left - height[i];
+                if (height[right] > maxRight)
+                    maxRight = height[right];
+                else
+                {
+                    var currentWater = maxRight - height[right];
+                    if (currentWater >= 0)
+                        totalWater += currentWater;
+                }
+                right--;
             }
         }
-        for(int i = height.Length - 1; i > maxI; i--)
-        {
-            if(height[i] > right)
-            {
-                right = height[i];
-            }
-            else
-            {
-                res += right - height[i];
-            }
-        }
-        return res;
+
+        return totalWater;
     }
 }
